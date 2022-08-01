674f4126601da5396dd7278ce68158c56430c07a
inderjit87 <inderjitsingh@somaiya.edu>
2022-08-01 04:48:09 +0000

    updated file status

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -20,9 +20,9 @@ def clock_gen(signal):
         signal.value <= 1
         yield Timer(1) 
 
-# Sample Test
+# Test 1
 @cocotb.test()
-def run_test(dut):
+def run_test_1(dut):
 
     # clock
     cocotb.fork(clock_gen(dut.CLK))
@@ -32,7 +32,7 @@ def run_test(dut):
     yield Timer(10) 
     dut.RST_N.value <= 1
 
-    ######### CTB : Test to expose the bug #############
+    ######### CTB : Test 1 to expose the bug #############
     # input transaction
     mav_putvalue_src1 = 0x5
     mav_putvalue_src2 = 0x0
@@ -56,7 +56,66 @@ def run_test(dut):
 
     cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
     cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
-    
+    print(f'SRC1 value in binary: {dut.mav_putvalue_src1.value}')
+    print(f'SRC1 value in hex: {hex(dut.mav_putvalue_src1.value)}')
+    print(f'SRC2 value in binary: {dut.mav_putvalue_src2.value}')
+    print(f'SRC2 value in hex: {hex(dut.mav_putvalue_src2.value)}')
+    print(f'SRC3 value in binary: {dut.mav_putvalue_src3.value}')
+    print(f'SRC3 value in hex: {hex(dut.mav_putvalue_src3.value)}')
+    print(f'Instruction value in binary: {dut.mav_putvalue_instr.value}')
+    print(f'Instruction value in hex: {hex(dut.mav_putvalue_instr.value)}')
     # comparison
     error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
     assert dut_output == expected_mav_putvalue, error_message
+
+
+    
+    
+# Test 2   
+@cocotb.test()
+def run_test_2(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(20) 
+    dut.RST_N.value <= 1
+
+    ######### CTB : Test 2 to expose the bug #############
+    # input transaction
+    mav_putvalue_src1 = 0xffffffff
+    mav_putvalue_src2 = 0x3
+    mav_putvalue_src3 = 0x0
+    mav_putvalue_instr = 0x40007033
+
+    # expected output from the model
+    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+    # driving the input transaction
+    dut.mav_putvalue_src1.value = mav_putvalue_src1
+    dut.mav_putvalue_src2.value = mav_putvalue_src2
+    dut.mav_putvalue_src3.value = mav_putvalue_src3
+    dut.EN_mav_putvalue.value = 1
+    dut.mav_putvalue_instr.value = mav_putvalue_instr
+  
+    yield Timer(21) 
+
+    # obtaining the output
+    dut_output = dut.mav_putvalue.value
+
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+    print(f'SRC1 value in binary: {dut.mav_putvalue_src1.value}')
+    print(f'SRC1 value in hex: {hex(dut.mav_putvalue_src1.value)}')
+    print(f'SRC2 value in binary: {dut.mav_putvalue_src2.value}')
+    print(f'SRC2 value in hex: {hex(dut.mav_putvalue_src2.value)}')
+    print(f'SRC3 value in binary: {dut.mav_putvalue_src3.value}')
+    print(f'SRC3 value in hex: {hex(dut.mav_putvalue_src3.value)}')
+    print(f'Instruction value in binary: {dut.mav_putvalue_instr.value}')
+    print(f'Instruction value in hex: {hex(dut.mav_putvalue_instr.value)}')
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    assert dut_output == expected_mav_putvalue, error_message
+
